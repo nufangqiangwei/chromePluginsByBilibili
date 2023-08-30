e20cca0aee7e76bf5267c201458194f7d5b55075
qiangwei <email.com>
2023-08-30 19:55:49 +0800

    修改完成

--- injected.js
+++ injected.js
@@ -1,276 +1,22 @@
+pageIndex = 1
 
-videoList = []
 class videoInfo {
     constructor() {
-        this.basic = {
-            "comment_id_str": String,
-            "comment_type": 1,
-            "like_icon": {
-                "action_url": "",
-                "end_url": "",
-                "id": 0,
-                "start_url": ""
-            },
-            "rid_str": "914256444"
-        }
-        this.id_str = "826301989233098772"
-        this.modules = {
-            "module_author": {
-                "avatar": {
-                    "container_size": {
-                        "height": 1.35,
-                        "width": 1.35
-                    },
-                    "fallback_layers": {
-                        "is_critical_group": true,
-                        "layers": [
-                            {
-                                "general_spec": {
-                                    "pos_spec": {
-                                        "axis_x": 0.675,
-                                        "axis_y": 0.675,
-                                        "coordinate_pos": 2
-                                    },
-                                    "render_spec": {
-                                        "opacity": 1
-                                    },
-                                    "size_spec": {
-                                        "height": 1,
-                                        "width": 1
-                                    }
-                                },
-                                "layer_config": {
-                                    "is_critical": true,
-                                    "tags": {
-                                        "AVATAR_LAYER": {},
-                                        "GENERAL_CFG": {
-                                            "config_type": 1,
-                                            "general_config": {
-                                                "web_css_style": {
-                                                    "borderRadius": "50%"
-                                                }
-                                            }
-                                        }
-                                    }
-                                },
-                                "resource": {
-                                    "res_image": {
-                                        "image_src": {
-                                            "placeholder": 6,
-                                            "remote": {
-                                                "bfs_style": "widget-layer-avatar",
-                                                "url": "https://i1.hdslb.com/bfs/face/c19a6bc8b889b9d37ee9d2804c1367b6c2ef1ea1.jpg"
-                                            },
-                                            "src_type": 1
-                                        }
-                                    },
-                                    "res_type": 3
-                                },
-                                "visible": true
-                            },
-                            {
-                                "general_spec": {
-                                    "pos_spec": {
-                                        "axis_x": 0.8000000000000002,
-                                        "axis_y": 0.8000000000000002,
-                                        "coordinate_pos": 1
-                                    },
-                                    "render_spec": {
-                                        "opacity": 1
-                                    },
-                                    "size_spec": {
-                                        "height": 0.41666666666666663,
-                                        "width": 0.41666666666666663
-                                    }
-                                },
-                                "layer_config": {
-                                    "tags": {
-                                        "GENERAL_CFG": {
-                                            "config_type": 1,
-                                            "general_config": {
-                                                "web_css_style": {
-                                                    "background-color": "rgb(255,255,255)",
-                                                    "border": "2px solid rgba(255,255,255,1)",
-                                                    "borderRadius": "50%",
-                                                    "boxSizing": "border-box"
-                                                }
-                                            }
-                                        },
-                                        "ICON_LAYER": {}
-                                    }
-                                },
-                                "resource": {
-                                    "res_image": {
-                                        "image_src": {
-                                            "local": 3,
-                                            "src_type": 2
-                                        }
-                                    },
-                                    "res_type": 3
-                                },
-                                "visible": true
-                            }
-                        ]
-                    },
-                    "mid": "920713"
-                },
-                "face": "https://i1.hdslb.com/bfs/face/c19a6bc8b889b9d37ee9d2804c1367b6c2ef1ea1.jpg",
-                "face_nft": false,
-                "following": true,
-                "jump_url": "//space.bilibili.com/920713/dynamic",
-                "label": "",
-                "mid": 920713,
-                "name": "爆裂菊是也",
-                "official_verify": {
-                    "desc": "",
-                    "type": 0
-                },
-                "pendant": {
-                    "expire": 0,
-                    "image": "",
-                    "image_enhance": "",
-                    "image_enhance_frame": "",
-                    "name": "",
-                    "pid": 0
-                },
-                "pub_action": "发布了动态视频",
-                "pub_location_text": "",
-                "pub_time": "昨天 17:13",
-                "pub_ts": 1691226824,
-                "type": "AUTHOR_TYPE_NORMAL",
-                "vip": {
-                    "avatar_subscript": 1,
-                    "avatar_subscript_url": "",
-                    "due_date": 1718121600000,
-                    "label": {
-                        "bg_color": "#FB7299",
-                        "bg_style": 1,
-                        "border_color": "",
-                        "img_label_uri_hans": "https://i0.hdslb.com/bfs/activity-plat/static/20220608/e369244d0b14644f5e1a06431e22a4d5/0DFy9BHgwE.gif",
-                        "img_label_uri_hans_static": "https://i0.hdslb.com/bfs/vip/8d7e624d13d3e134251e4174a7318c19a8edbd71.png",
-                        "img_label_uri_hant": "",
-                        "img_label_uri_hant_static": "https://i0.hdslb.com/bfs/activity-plat/static/20220614/e369244d0b14644f5e1a06431e22a4d5/uckjAv3Npy.png",
-                        "label_theme": "annual_vip",
-                        "path": "",
-                        "text": "年度大会员",
-                        "text_color": "#FFFFFF",
-                        "use_img_label": true
-                    },
-                    "nickname_color": "#FB7299",
-                    "status": 1,
-                    "theme_type": 0,
-                    "type": 2
-                }
-            },
-            "module_dynamic": {
-                "additional": null,
-                "desc": {
-                    "rich_text_nodes": [
-                        {
-                            "orig_text": "主持人借我玩了会儿猫耳朵~",
-                            "text": "主持人借我玩了会儿猫耳朵~",
-                            "type": "RICH_TEXT_NODE_TYPE_TEXT"
-                        }
-                    ],
-                    "text": "主持人借我玩了会儿猫耳朵~"
-                },
-                "major": {
-                    "archive": {
-                        "aid": "914256444",
-                        "badge": {
-                            "bg_color": "#FB7299",
-                            "color": "#FFFFFF",
-                            "icon_url": null,
-                            "text": "动态视频"
-                        },
-                        "bvid": "BV1UM4y1H7EL",
-                        "cover": "http://i0.hdslb.com/bfs/archive/72221969be85a3e4bbc435ffc4f3708e4f26c322.png",
-                        "desc": "主持人借我玩了会儿猫耳朵~",
-                        "disable_preview": 0,
-                        "duration_text": "00:15",
-                        "jump_url": "//www.bilibili.com/video/BV1UM4y1H7EL/",
-                        "stat": {
-                            "danmaku": "36",
-                            "play": "7.9万"
-                        },
-                        "title": "主持人借我玩了会儿猫耳朵~",
-                        "type": 1
-                    },
-                    "type": "MAJOR_TYPE_ARCHIVE"
-                },
-                "topic": null
-            },
-            "module_interaction": {
-                "items": [
-                    {
-                        "desc": {
-                            "rich_text_nodes": [
-                                {
-                                    "orig_text": "风羽沉：",
-                                    "rid": "193523998",
-                                    "text": "风羽沉：",
-                                    "type": "RICH_TEXT_NODE_TYPE_AT"
-                                },
-                                {
-                                    "orig_text": "你怎么知道我今天拿到菊花花老师的签售啦",
-                                    "text": "你怎么知道我今天拿到菊花花老师的签售啦",
-                                    "type": "RICH_TEXT_NODE_TYPE_TEXT"
-                                },
-                                {
-                                    "orig_text": "[哇]",
-                                    "text": "[哇]",
-                                    "type": "RICH_TEXT_NODE_TYPE_TEXT"
-                                },
-                                {
-                                    "orig_text": "[哇]",
-                                    "text": "[哇]",
-                                    "type": "RICH_TEXT_NODE_TYPE_TEXT"
-                                }
-                            ],
-                            "text": "你怎么知道我今天拿到菊花花老师的签售啦[哇][哇]"
-                        },
-                        "type": 1
-                    }
-                ]
-            },
-            "module_more": {
-                "three_point_items": [
-                    {
-                        "label": "取消关注",
-                        "type": "THREE_POINT_FOLLOWING"
-                    },
-                    {
-                        "label": "举报",
-                        "type": "THREE_POINT_REPORT"
-                    }
-                ]
-            },
-            "module_stat": {
-                "comment": {
-                    "count": 516,
-                    "forbidden": false
-                },
-                "forward": {
-                    "count": 1,
-                    "forbidden": false
-                },
-                "like": {
-                    "count": 13006,
-                    "forbidden": false,
-                    "status": false
-                }
-            }
-        }
-        this.type = "DYNAMIC_TYPE_AV"
-        this.visible = true
+        this.AuthorName = ""
+        this.AuthorUrl = ""
+        this.AuthorUuid = ""
+        this.CoverUrl = ""
+        this.PushTime = 0
+        this.Title = ""
+        this.Url = ""
+        this.VideoUuid = ""
+        this.WebSite = ""
     }
 }
 
 function clickTo(url) {
     //跳转新页面
-    return () => {
-        window.open(url, "_blank")
-    }
+    window.open(url, "_blank")
 }
 
 function generationVideoCard(videoInfoData) {
@@ -278,8 +24,11 @@ function generationVideoCard(videoInfoData) {
     videoElement.classList.add("pluginVideo");
 
     const videoImage = document.createElement("img");
-    videoImage.src = videoInfoData.image;
-    videoImage.alt = videoInfoData.title;
+    videoImage.src = videoInfoData.CoverUrl;
+    videoImage.alt = videoInfoData.Title;
+    videoImage.addEventListener("click", () => {
+        clickTo(`https://www.bilibili.com/video/${videoInfoData.VideoUuid}`);
+    });
 
     const videoInfo = document.createElement("div");
     videoInfo.classList.add("pluginVideoInfo");
@@ -294,9 +43,9 @@ function generationVideoCard(videoInfoData) {
 
     const videoViews = document.createElement("a");
     videoViews.classList.add("pluginVideoAuthor");
-    videoViews.textContent = "观看次数: " + videoInfoData.views;
+    videoViews.textContent = videoInfoData.AuthorName;
     videoViews.addEventListener("click", () => {
-        console.log(videoInfoData.views);
+        clickTo(`https://space.bilibili.com/${videoInfoData.AuthorUuid}`);
     });
 
     videoInfo.appendChild(videoTitle);
@@ -309,7 +58,21 @@ function generationVideoCard(videoInfoData) {
 }
 
 
+function dealWithResponse(response) {
+    for (const info of response.data) {
+        videoGrid.appendChild(generationVideoCard(info))
+    }
+}
 
+function getVideo() {
+    let xhr = new XMLHttpRequest()
+    xhr.open("GET", `http://127.0.0.1:8000/getVideoList?page=${pageIndex}`)
+    xhr.addEventListener("load", (e) => {
+        dealWithResponse(JSON.parse(xhr.responseText))
+        pageIndex++
+    })
+    xhr.send()
+}
 
 videoMotion = document.getElementsByClassName('bili-dyn-list')
 leftDom = document.getElementsByClassName('left')
@@ -321,23 +84,18 @@ if (videoMotion.length > 0) {
     let dynList = videoMotion[0]
     let section = dynList.parentNode
 
-    mainDom[0].style.width = (window.screen.width*0.9) + 'px'
+    mainDom[0].style.width = (window.screen.width * 0.9) + 'px'
     dynList.remove()
     rightDom[0].remove()
     leftDom[0].remove()
     section.appendChild(videoGrid)
-
+    getVideo()
     // 触底加载列表
     // document.body.onscroll = loadDynamicVideo
 }
 
-async function appendVideoList(dynamicResponseData) {
-    for (const videoInfoData of dynamicResponseData.data.items) {
-        videoGrid.appendChild(generationVideoCard(videoInfoData))
-    }
-}
 
-(function(xhr) {
+(function (xhr) {
 
     var XHR = XMLHttpRequest.prototype;
 
@@ -345,7 +103,7 @@ async function appendVideoList(dynamicResponseData) {
     var send = XHR.send;
     var setRequestHeader = XHR.setRequestHeader;
 
-    XHR.open = function(method, url) {
+    XHR.open = function (method, url) {
         this._method = method;
         this._url = url;
         this._requestHeaders = {};
@@ -354,25 +112,25 @@ async function appendVideoList(dynamicResponseData) {
         return open.apply(this, arguments);
     };
 
-    XHR.setRequestHeader = function(header, value) {
+    XHR.setRequestHeader = function (header, value) {
         this._requestHeaders[header] = value;
         return setRequestHeader.apply(this, arguments);
     };
 
-    XHR.send = function(postData) {
+    XHR.send = function (postData) {
 
-        this.addEventListener('load', function() {
+        this.addEventListener('load', function () {
             var endTime = (new Date()).toISOString();
 
             var myUrl = this._url ? this._url.toLowerCase() : this._url;
-            if(myUrl) {
+            if (myUrl) {
 
                 if (postData) {
                     if (typeof postData === 'string') {
                         try {
                             // here you get the REQUEST HEADERS, in JSON format, so you can also use JSON.parse
                             this._requestHeaders = postData;
-                        } catch(err) {
+                        } catch (err) {
                             console.log('Request Header JSON decode failed, transfer_encoding field could be base64');
                             console.log(err);
                         }
@@ -383,9 +141,9 @@ async function appendVideoList(dynamicResponseData) {
 
                 // here you get the RESPONSE HEADERS
                 var responseHeaders = this.getAllResponseHeaders();
-                if (this._url.startsWith('//api.bilibili.com/x/polymer/web-dynamic/v1/feed/all')){
+                if (this._url.startsWith('//api.bilibili.com/x/polymer/web-dynamic/v1/feed/all')) {
                     // console.log(this.responseText)
-                    appendVideoList(JSON.parse(this.responseText))
+                    getVideo()
                 }
                 // if ( this.responseType != 'blob' && this.responseText) {
                 //     // responseText is string or null
