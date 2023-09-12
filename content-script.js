cea3299523da8ab67a31e8d208de2e6ed94d4ed7
ming.ni <ming.ni@shunwang.com>
2023-09-12 16:20:07 +0800

    打包版本

--- content-script.js
+++ content-script.js
@@ -1,3 +1,10 @@
-let s = document.createElement('script')
-s.src = chrome.runtime.getURL('injected.js')
-(document.head || document.documentElement).appendChild(s)
+function injectedJS() {
+    let s = document.createElement('script')
+    s.src = chrome.runtime.getURL('injected.js')
+    s.onload = function () {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(s)
+}
+
+injectedJS()
